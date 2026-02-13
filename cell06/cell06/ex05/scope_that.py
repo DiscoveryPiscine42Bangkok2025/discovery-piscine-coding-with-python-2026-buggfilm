365e0739fb8d17230ecff6523f4bd4b2faba8f90
buggfilm <ratthaphums48@gmail.com>
2026-02-13 23:13:34 +0700

    Add files via upload

---
+++ cell06/cell06/ex05/scope_that.py
@@ -0,0 +1,13 @@
+def add_one(x):
+    x = x + 1
+    print("Inside function:", x)
+
+
+# main program
+number = 5
+
+print("Before function:", number)
+
+add_one(number)
+
+print("After function:", number)
